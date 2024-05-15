9f6ab1071e8c230372b9bae7c9335132ad9dd574
xuanmanh <90127093+xmak9102@users.noreply.github.com>
2024-05-15 13:25:10 +0700

    Update train.py

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Training')
     parser.add_argument('--entity', type=str,
-                        help='Entity', default='davidlainesv')
+                        help='Entity', default='xmak9102')
     parser.add_argument('--project', type=str,
                         help='Project name', default='DenseNet_project')
     parser.add_argument('--dataset', type=str,
